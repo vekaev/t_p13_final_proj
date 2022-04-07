8ffb127a5a586fa3ab4a998a8e6666d01a0bc2d7
ypetrenko <vekaev4@gmail.com>
2022-04-07 19:57:24 +0300

    lesson-4

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,24 @@
-console.log("hello")
+console.log('He')
+
+const BASE_URL = 'http://127.0.0.1:8000';
+
+const form_name = document.getElementById("form_name")?.value;
+const form_phone = document.getElementById("form_phone")?.value;
+const form_email = document.getElementById("form_email")?.value;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const paramsString = new URLSearchParams({
+    name: form_name,
+    phone: form_phone,
+    email: form_email
+  });
+  const url = `${BASE_URL}/client_form?${paramsString}`;
+
+  window.fetch(url).then(response.json)
+  .then(function(data) {
+    console.log(data);
+  });
+
+  return false;
+}
